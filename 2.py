4cd1be638aa7bde058565102aada4f2f95e7288f
aliabbas <aliabbas4k12@gmail.com>
2020-06-04 10:50:28 +0530

    day-1 programs

---
+++ 2.py
@@ -0,0 +1,10 @@
+#Day-1
+#Program-2
+
+
+#input
+
+import math
+number = int(input("Enter Number Whose Square root You want To Find"))
+ans = math.sqrt(number)
+print("The Square root of %d is " %(number),ans)
